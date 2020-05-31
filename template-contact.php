5f63155a7d8a00b512b70861ef9dd2db3b82aaa8
Vladimir Vuckovic <vladimirvckv@gmail.com>
2020-05-31 14:22:56 +0200

    Kontakt forma na kontakt strani

--- template-contact.php
+++ template-contact.php
@@ -69,70 +69,8 @@ get_header(); ?>
 					
 					<div class="row">
 						<div class="col-md-12">
-							<h3>Contact Form</h3>
-							<form action="php/contact-form.php.json" id="contact-form">
-
-								<div class="alert alert-success hidden" id="contact-alert-success">
-									<strong>Success!</strong> Thank you for your message. Reply will be in a while!
-								</div>
-								<div class="alert alert-danger hidden" id="contact-alert-error">
-									<strong>Error!</strong> Something went wrong sending your message.
-								</div>
-
-								<div class="row">
-									<div class="col-md-4">
-										<div class="form-group">
-											<label>Name <span class="required">*</span></label>
-											<input type="text"
-												value=""
-												data-msg-required="Please enter your name."
-												class="form-control"
-												name="name" id="name">
-										</div>
-									</div>
-									<div class="col-md-4">
-										<div class="form-group">
-											<label>Email <span class="required">*</span></label>
-											<input type="email" 
-												value=""
-												data-msg-required="Please enter your email address."
-												data-msg-email="Please enter a valid email address."
-												class="form-control"
-												name="email"
-												id="email">
-										</div>
-									</div>
-									<div class="col-md-4">
-										<div class="form-group">
-											<label>Subject</label>
-											<input type="text" 
-												value=""
-												data-msg-required="Please enter the subject."
-												class="form-control"
-												name="subject"
-												id="subject">
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<div class="form-group">
-											<label>Message <span class="required">*</span></label>
-											<textarea
-												data-msg-required="Please enter your message."
-												rows="10"
-												class="form-control"
-												name="message"
-												id="message"></textarea>
-										</div>
-									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<input type="submit" value="Send Message" class="btn btn-primary" data-loading-text="Loading...">
-									</div>
-								</div>
-							</form>
+							<h3>Kontaktirajte nas</h3>
+							<?php echo do_shortcode('[contact-form-7 id="174" title="Kontakt strana"]'); ?>
 						</div>
 					</div>
 
